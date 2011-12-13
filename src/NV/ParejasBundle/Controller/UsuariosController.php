afdbabb418b55dce78f025f1045161bb3f5cfa88
Israel Noguera <israelnoguera@yahoo.com>
2011-12-14 00:32:27 +0100

    cambios desde pc home asus

--- src/NV/ParejasBundle/Controller/UsuariosController.php
+++ src/NV/ParejasBundle/Controller/UsuariosController.php
@@ -11,7 +11,7 @@ class UsuariosController extends Controller{
         //CONSULTA MEDIANTE DQL Y REPOSITORIO (metodo aconsejado)
         $em = $this->getDoctrine()->getEntityManager();
         $usuarios = $em->getRepository('NVParejasBundle:Usuarios')
-            ->findAllOrderedById();
+            ->findAllUsers();
 
         $arrParams = array(
             'mainmenu' => 'usuarios',
--- src/NV/ParejasBundle/Repository/UsuariosRepository.php
+++ src/NV/ParejasBundle/Repository/UsuariosRepository.php
@@ -5,16 +5,27 @@ namespace NV\ParejasBundle\Repository;
 use Doctrine\ORM\EntityRepository;
 
 class UsuariosRepository extends EntityRepository{
-    
-    public function findAllOrderedById(){
-        
+
+    public function findAllUsers($order='id'){        
         return $this->getEntityManager()
-            ->createQuery('SELECT p FROM NVParejasBundle:Usuarios p WHERE p.username != :value ORDER BY p.id DESC')
-            ->setParameter('value', '')
-            ->getResult();        
-    
+            ->createQuery('
+                    SELECT p 
+                    FROM NVParejasBundle:Usuarios p 
+                    ORDER BY p.'.$order.' DESC
+                    ')
+            ->getResult();            
     }
  
-    
+    public function getAnuncios($id_usuario){
+        return $this->getEntityManager()
+            ->createQuery('
+                    SELECT p 
+                    FROM NVParejasBundle:Anuncios p
+                    WHERE usuario_id = :idusuario
+                    ORDER BY p.fechahora DESC
+                    ')
+                ->setParameter('idusuario', $id_usuario)
+            ->getResult();        
+    }
     
 }
